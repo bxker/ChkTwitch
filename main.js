550856a4bb49dadff3ba4a969e046751d09af8ff
bxker <jake.luke.baker@gmail.com>
2018-06-18 22:24:05 -0400

    testing js

--- main.js
+++ main.js
@@ -1,27 +1,21 @@
-function getChannelId() {
-   
-    //Rewritten to use fetch rather than ajax
+var TwitchApi = require('twitch-api')
+var twitch = new TwitchApi({
+    clientId: 'Rekabik'
+    clientSecret: 'oauth:4datynltw9owu3454zyc78uzypcv01'
+    redirectUri: 'same redirectUri that you have configured on your app',
+    scopes: [array of scopes you want access to]
 
-    //Some detailed comments so I remember what I did
-    //Set up the request, we need url, method and headers which are defined as globals to aid reuse
-    var request = new Request('https://api.twitch.tv/kraken/search/channels?query=' + channel, {
-        headers: v5headers,
-        method: 'GET'
-    });
-
-    //Make the call using fetch, which returns a promise
-    //When the fetch succeeds return the response as json
-    //Then manage the data it contained
-    //Catch just in case
-    fetch(request).then(function(response) {
-        //Success, return the response as JSON
-        return response.json();
-    }).then(function (data){
-        //Data contains the JSON from the response
-        //Update the global variable with the channel id rather than the name
-        channel = data["channels"][0]["_id"];
-        //Now we can get the current game
-        getCurrentGame();
-    }).catch(function (err) {
-       console.log(err);
-    });
+})
+twitch.getAccessToken(code, function(err, body){
+    if (err){
+        console.log(err);
+    }
+    else{
+      /*
+      * body = {
+      *   access_token: 'your authenticated user access token',
+      *   scopes: [array of granted scopes]
+      * }
+      */  
+    }
+})
